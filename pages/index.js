b74e8706cdcec923d243432a9cce241b3661dae3
tay59 <tayronne339@gmail.com>
2025-07-20 09:11:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
+import Head from 'next/head';
+import ClipEditor from '../components/ClipEditor';
 
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Clipmaster T</title>
+      </Head>
+      <main>
+        <h1>🎬 Clipmaster T</h1>
+        <ClipEditor />
+      </main>
+    </div>
+  );
+}
